78fc3da153afc466b55fcc387ca950f8b53765da
Mahmoud Elsheikh <melsheikh92@gmail.com>
2024-01-03 15:25:28 +0300

    Fixed bumping version strategy

--- bump_version.sh
+++ bump_version.sh
@@ -9,10 +9,10 @@ NEW_VERSION=$(echo $CURRENT_VERSION + 0.1 | bc)
 # Update the appversion file with the new version
 echo $NEW_VERSION > appversion
 
-# Update the values.YAML file with the new version
-YAML_FILE="charts/values.yaml"  
+# Update the YAML file with the new version
+YAML_FILE="./charts/values.yaml"  
 
-# Use sed to replace the version in the values.YAML file
+# Use sed to replace the version in the YAML file
 sed -i "s/\(image:\s*tag:\s*\)\"[^\"]*\"/\1\"$NEW_VERSION\"/" $YAML_FILE
 
 # Commit the changes
@@ -22,3 +22,6 @@ git commit -m "Bump version to $NEW_VERSION"
 # Tag the commit
 TAG_NAME="v$NEW_VERSION"
 git tag $TAG_NAME
+
+# Push changes to the remote repository
+git push origin main --tags
