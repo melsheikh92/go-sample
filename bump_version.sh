0d44405ea536f0172be73380b76e5023b5ba1177
Mahmoud Elsheikh <melsheikh92@gmail.com>
2024-01-03 14:04:55 +0300

    Added bump version script

---
+++ bump_version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Read the current version from appversion file
+CURRENT_VERSION=$(cat appversion)
+
+# Bump the version
+NEW_VERSION=$(echo $CURRENT_VERSION + 0.1 | bc)
+
+# Update the appversion file with the new version
+echo $NEW_VERSION > appversion
+
+# Update the values.YAML file with the new version
+YAML_FILE="charts/values.yaml"  
+
+# Use sed to replace the version in the values.YAML file
+sed -i "s/\(image:\s*tag:\s*\)\"[^\"]*\"/\1\"$NEW_VERSION\"/" $YAML_FILE
+
+# Commit the changes
+git add appversion $YAML_FILE
+git commit -m "Bump version to $NEW_VERSION"
