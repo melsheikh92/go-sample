98aa45749ffdfeb22151ebc9ab75d49d7a7bd438
Mahmoud Elsheikh <melsheikh92@gmail.com>
2024-01-03 16:20:25 +0300

    Update bump_version.sh

--- ci/bump_version.sh
+++ ci/bump_version.sh
@@ -10,18 +10,18 @@ NEW_VERSION=$(echo $CURRENT_VERSION + 0.1 | bc)
 echo $NEW_VERSION > appversion
 
 # Update the YAML file with the new version
-YAML_FILE="./charts/values.yaml"  
+YAML_FILE="charts/values.yaml"  
 
 # Use sed to replace the version in the YAML file
-sed -i "s/\(image:\s*tag:\s*\)\"[^\"]*\"/\1\"$NEW_VERSION\"/" $YAML_FILE
+sed -i "s/\(tag:\s*\)\"[^\"]*\"/\1\"$NEW_VERSION\"/" $YAML_FILE
 
 # Commit the changes
 git add appversion $YAML_FILE
+
+# Commit the changes and tag
+git add appversion $YAML_FILE
 git commit -m "Bump version to $NEW_VERSION"
+git tag -a "v$NEW_VERSION" -m "Version $NEW_VERSION"
 
-# Tag the commit
-TAG_NAME="v$NEW_VERSION"
-git tag $TAG_NAME
-
-# Push changes to the remote repository
-git push origin main
+# Push changes and tags to the remote repository
+git push origin main --tags
